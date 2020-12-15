62e8f473af32456e8e9b5ed4bd9c12ea04c56a0e
Paul Hahn <paul.hahn@azblue.com>
2020-12-15 15:04:40 -0700

    Added windows specific file.

---
+++ custom/setup-windows.el
@@ -0,0 +1,15 @@
+;;; initfile --- Summary:
+;;; Commentary:
+;; Emacs 26 and newer
+
+;; On windows, use hunspell
+(setq-default ispell-program-name "hunspell")
+(custom-set-variables '(pandoc-binary "c:/Users/phahn01/AppData/Local/Pandoc/pandoc.exe"))
+;;(custom-set-variables '(markdown-command "c:/Users/phahn01/AppData/Local/Pandoc/pandoc.exe"))
+(setq markdown-command "c:/Users/phahn01/AppData/Local/Pandoc/pandoc.exe")
+
+
+;; On Windows, set default to unix line endings.
+(setq default-buffer-file-coding-system 'utf-8-unix)
+
+(provide 'setup-windows)
